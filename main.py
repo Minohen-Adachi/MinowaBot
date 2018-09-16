3612f4e0390681b3d3c2c17564d51db7088f422a
Atsuyuki Adachi <38243880+adatsudasu@users.noreply.github.com>
2018-09-16 17:45:43 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -58,8 +58,7 @@ resDictionary = {
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	print("=== handle_message ===")
-	
+    print("=== handle_message ===")
     # メッセージが送られてきたときの処理 
     # 辞書に含まれるものは特定の言葉を返す 
     for key, value in resDictionary.items():
