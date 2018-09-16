1524cb2e65f2bc1b7cf547d2432372888bc42f67
Atsuyuki Adachi <38243880+adatsudasu@users.noreply.github.com>
2018-09-16 16:42:28 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -21,17 +21,19 @@ handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    print("=== callback ===")
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+
     # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
+
     # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
         abort(400)
+
     return 'OK'
 
 
@@ -40,8 +42,6 @@ randomResList = []
 
 # random.txtから名言を読み込む
 with open('random.txt', 'r') as f:
-	print("=== open ===")
-
     # 一列ごとに読み込む
     for line in f:
         # 改行文字の削除
@@ -58,7 +58,6 @@ resDictionary = {
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-	print("=== handle_message ===")
 
     # メッセージが送られてきたときの処理
 
